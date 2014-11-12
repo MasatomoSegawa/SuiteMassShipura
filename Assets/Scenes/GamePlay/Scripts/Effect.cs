c9ad796486014d206870a466fc4d3524b85c6c8e
Masatomo Segawa <masa-segawa@masa-segawa.com>
2014-11-12 18:20:59 +0900

    no message

---
+++ Assets/Scenes/GamePlay/Scripts/Effect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Effect : MonoBehaviour {
+
+	void EndAnimation(){
+
+		if (this.transform.parent.gameObject != null) {
+			DestroyImmediate (this.transform.parent.gameObject);
+		} else {
+			DestroyImmediate (this.gameObject);
+		}
+
+	}
+
+}
